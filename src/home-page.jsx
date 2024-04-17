169593e44fcf47de39507559aa3f9debb0a6a820
Aris Lemmenes <aris.lemmenes@gmail.com>
2024-04-17 13:08:53 -0700

    Big changes

--- src/home-page.jsx
+++ src/home-page.jsx
@@ -1,21 +1,43 @@
 import koreanalphabet from './Korean/Alphabet.json'
+import koreansentences from './Korean/sentences-1.json'
+import koreanwords from './Korean/words.json'
 import { useState } from 'react';
 import { Button, ButtonGroup,  Text, Box } from '@chakra-ui/react';
 import './home-page.css'
+import Cookies from 'js-cookie';
 
 
 
 
 
 function HomePage() {
-
+  if (Cookies.get("level") == null){
+    Cookies.set("level",0);
+  }
+  const [level, setLevel] = useState(parseInt(Cookies.get("level")));
   const [options, setOptions] = useState(generateOptions());
+  
   
   function generateOptions() {
     const randomIndex = Math.floor(Math.random() * koreanalphabet.characters.length);
-    const korean = koreanalphabet.characters[randomIndex].korean;
-    const correctEnglish = koreanalphabet.characters[randomIndex].english;
-    const allEnglishOptions = koreanalphabet.characters.map(item => item.english);
+    let korean = "";
+    let correctEnglish = "";
+    let allEnglishOptions = "";
+    if (level  >= 1000){
+      korean = koreansentences.sentences[randomIndex].korean;
+      correctEnglish = koreansentences.sentences[randomIndex].english;
+      allEnglishOptions = koreansentences.sentences.map(item => item.english);
+    }
+    else if (level >=20){
+      korean = koreanwords.words[randomIndex].korean;
+      correctEnglish = koreanwords.words[randomIndex].english;
+      allEnglishOptions = koreanwords.words.map(item => item.english);
+    }else{
+      korean = koreanalphabet.characters[randomIndex].korean;
+      correctEnglish = koreanalphabet.characters[randomIndex].english;
+      allEnglishOptions = koreanalphabet.characters.map(item => item.english);
+    }
+    
     const incorrectEnglish = allEnglishOptions
       .filter(option => option !== correctEnglish)
       .sort(() => 0.5 - Math.random())
@@ -33,8 +55,13 @@ function HomePage() {
     const isCorrect = selectedOption === options.correctEnglish;
     if (isCorrect) {
       setOptions(generateOptions());
+      setLevel(level + 1)
+      Cookies.set("level",level)
       speak(options.korean, "ko-KR")
     } else {
+      if (level >= 1){
+        setLevel(level - 1)
+      }
       speak("부정확한", "ko-KR")
     }
   }
@@ -43,8 +70,9 @@ function HomePage() {
   }
   return (
     <Box alignItems="center"  justifyContent="center" display="flex" flexDirection="column">
-        <Text mt = {-300}  fontSize="9xl">한글</Text>
-        <Text mt = {-6} size = "small" fontSize="1xl" mb={4}>The Korean Alphabet</Text>
+        <Text mt = {-300}  fontSize="9xl">한국어를 배우다</Text>
+        <Text mt = {-6} size = "small" fontSize="1xl" mb={4}>Learn Korean</Text>
+        <Text mt = {-6} size = "small" fontSize="1xl" mb={4}>{level}</Text>
         <Button  onClick={() => handleCharacterClick(options.korean)} size = "xxl" fontSize="9xl" mb={4}>{options.korean}</Button>
         <ButtonGroup>
           {options.options.map(option => (
